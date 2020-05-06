acef7b7e78a0251775ccd3b1bda01d38a1fdbeaf
bradydowling <bradydowling@gmail.com>
2020-05-06 11:18:56 -0400

    Formatting and file changes

---
+++ src/string-calculator.js
@@ -0,0 +1,16 @@
+const add = (inputString) => {
+  if (inputString === '') {
+    return 0;
+  }
+
+  let result = 0;
+  const inputStrings = inputString.split(',');
+
+  for (let i=0; i < inputStrings.length; i++) {
+    result += parseInt(inputStrings[i]);
+  }
+
+  return result;
+};
+
+window.stringCalculator = { add };
---
+++ test/string-calculator_test.js
@@ -0,0 +1,29 @@
+describe('stringCalculator', () => {
+  describe('when an empty string is passed in', () => {
+    it('returns 0', () => {
+      const result = stringCalculator.add('');
+      assert(result === 0);
+    });
+  });
+
+  describe('when a number is passed in', () => {
+    it('returns the number', () => {
+      const result = stringCalculator.add('2');
+      assert(result === 2);
+    });
+  });
+
+  describe('when string is passed in', () => {
+    it('returns NaN', () => {
+      const result = stringCalculator.add('a');
+      assert(isNaN(result));
+    });
+  });
+
+  describe('when "1,2" is passed in', () => {
+    it('returns 3', () => {
+      const result = stringCalculator.add('1,2');
+      assert(result === 3);
+    });
+  });
+});
